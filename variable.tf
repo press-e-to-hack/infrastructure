fe8abe7a859d85188b626e339e672deadd74fcce
Lucas Ramage <oxr463@gmx.us>
2020-02-13 14:13:14 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -11,3 +11,10 @@ variable "ubuntu" {
   description = "Default LTS"
   default     = "ubuntu-18-04-x64"
 }
+
+# GitHub Organization
+
+variable "github_organization" {
+  description = "GitHub Organization"
+  default     = "press-e-to-hack"
+}
