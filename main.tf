921bc7d90486b66c2cef46e60395d7f85c7f6fef
Lucas Ramage <oxr463@gmx.us>
2020-02-13 12:01:14 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,12 @@ provider "digitalocean" {
   # export DIGITALOCEAN_TOKEN="Your API TOKEN"
   #
 }
+
+resource "digitalocean_droplet" "tedicross" {
+  ssh_keys           = [26498168]
+  size               = "s-1vcpu-1gb"
+  private_networking = false
+  backups            = false
+  ipv6               = false
+  name               = "ubuntu-s-1vcpu-1gb-nyc1-01"
+}
